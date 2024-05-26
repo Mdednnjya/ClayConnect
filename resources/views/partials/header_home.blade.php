b3d7d1f5504aa55e97a1cd662cf9b7e9f624bbe2
Danan <narayanmade578@gmail.com>
2024-05-26 11:27:40 +0700

    Add homepage.blade

---
+++ resources/views/partials/header_home.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid px-5 py-4">
+        <div class="col-md-3">
+            <a class="navbar-brand" href="#" style="font-family: 'Encode Sans Semi Expanded'; font-size: 32px;" id="brand-logo">Clay-Connect</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1">
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#about" style="font-family: 'Lato'; font-size: 20px; text-decoration: none; color: black; transition: color 0.3s;" onmouseover="this.style.color='gray'" onmouseout="this.style.color='black'">About</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#goals" style="font-family: 'Lato'; font-size: 20px; text-decoration: none; color: black; transition: color 0.3s;" onmouseover="this.style.color='gray'" onmouseout="this.style.color='black'">Our Goals</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#location" style="font-family: 'Lato'; font-size: 20px; text-decoration: none; color: black; transition: color 0.3s;" onmouseover="this.style.color='gray'" onmouseout="this.style.color='black'">Location</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#contact" style="font-family: 'Lato'; font-size: 20px; text-decoration: none; color: black; transition: color 0.3s;" onmouseover="this.style.color='gray'" onmouseout="this.style.color='black'">Contact</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="" style="font-family: 'Scheherazade New'; font-size: 20px; font-weight: 600; text-decoration: none; color: white; background-color: #1a202c; transition: color 0.3s; padding: 6px 45px; border-radius: 25px" onmouseover="this.style.color='gray'" onmouseout="this.style.color='white'">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<script>
+    const brandLogo = document.getElementById('brand-logo');
+    const mediaQuery = window.matchMedia('(max-width: 576px)');
+
+    function handleMediaQueryChange(e) {
+        if (e.matches) {
+            brandLogo.style.fontSize = '22px';
+        } else {
+            brandLogo.style.fontSize = '32px';
+        }
+    }
+
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+</script>
