1ead66a9414811be881d335b3dc17dc33c72f9ab
Danan <narayanmade578@gmail.com>
2024-05-27 11:38:12 +0700

    Minor improvement for footer_auth

--- resources/views/partials/footer_auth.blade.php
+++ resources/views/partials/footer_auth.blade.php
@@ -1,9 +1,9 @@
-<footer class="">
+<footer style="box-shadow: 0 14px 19px rgba(0, 0, 0, 150);">
     <div class="container-fluid">
-      <div class="row p-4 p-sm-2 p-md-2 p-lg-2 p-xl-2">
-        <div class="col-12 text-sm-start text-center text-wrap d-block" style="font-family: 'Scheherazade New'; font-size: 18px">
-          COPYRIGHT © 2024 CLAYCONNECT ALL RIGHTS RESERVED.
+        <div class="row p-4 mx-3 p-sm-2 p-md-2 p-lg-2 p-xl-2">
+            <div class="col-12 p-3 text-start text-wrap d-block" style="font-family: 'Scheherazade New'; font-size: 18px">
+                COPYRIGHT © 2024 CLAYCONNECT ALL RIGHTS RESERVED.
+            </div>
         </div>
-      </div>
     </div>
 </footer>
---
+++ resources/views/partials/header_auth.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid px-5 py-4">
+        <div class="col-md-3">
+            <a class="navbar-brand" href="{{ url('/') }}" style="font-family: 'Encode Sans Semi Expanded'; font-size: 32px;" id="brand-logo">Clay-Connect</a>
+        </div>
+        @if(isset($isLoginPage) && $isLoginPage)
+            <a class="nav-link" href="register" style="font-family: 'Scheherazade New'; font-size: 20px; font-weight: 600; text-decoration: none; color: white; background-color: #1a202c; transition: color 0.3s; padding: 6px 45px; border-radius: 25px" onmouseover="this.style.color='gray'" onmouseout="this.style.color='white'">Get Started</a>
+        @else
+            <a class="nav-link" href="login" style="font-family: 'Scheherazade New'; font-size: 20px; font-weight: 600; text-decoration: none; color: white; background-color: #1a202c; transition: color 0.3s; padding: 6px 45px; border-radius: 25px" onmouseover="this.style.color='gray'" onmouseout="this.style.color='white'">Login</a>
+        @endif
+    </div>
+</nav>
+
+<script>
+    const brandLogo = document.getElementById('brand-logo');
+    const mediaQuery = window.matchMedia('(max-width: 576px)');
+
+    function handleMediaQueryChange(e) {
+        if (e.matches) {
+            brandLogo.style.fontSize = '22px';
+        } else {
+            brandLogo.style.fontSize = '32px';
+        }
+    }
+
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+</script>
