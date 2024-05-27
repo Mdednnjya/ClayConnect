2f93eb34d0efe6200a275e1df4fe2b5e50a3cbf3
Danan <narayanmade578@gmail.com>
2024-05-27 11:05:46 +0700

    Refactor header.blade for improved readability and layout consistency

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,9 +41,6 @@
 <body >
     @yield('content')
 
-    <footer>
-        @yield('footer', View::make('partials.footer_main'))
-    </footer>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ $googleMapsApiKey }}&callback=initMap" async defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
