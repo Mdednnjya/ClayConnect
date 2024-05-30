64c26078592cfe4b87e1e72d9c10f45ecfcedf4d
Danan <narayanmade578@gmail.com>
2024-05-30 09:14:34 +0700

    Add container for purchase history template in account subpage

--- resources/views/account/history.blade.php
+++ resources/views/account/history.blade.php
@@ -39,7 +39,73 @@
                     <div class="card" >
                         <div class="card-header" style="background-color: white; font-family: 'Scheherazade New'; font-size: 18px; font-weight: bold; border-bottom: 1px solid black;">History</div>
                         <div class="card-body">
-                            <p>Ini adalah halaman History.</p>
+                            <div class="container mb-5">
+                                <div class="card">
+                                    <div class="container p-4" style="font-family: 'Crimson Text'">
+                                        <div class="row p-3">
+                                            <div class="col-12 col-md-2">
+                                                <div class="py-3" style="background-color: #FFF5EB; display: flex; align-items: center; justify-content: center; width: auto; height: auto;">
+                                                    <img src="{{ asset('images/account/product-1.png') }}" alt="Photo Product" style="max-width: 100%; height: auto; display: block;">
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12 col-md-6">
+                                                <div class="row d-flex justify-content-between align-items-center">
+                                                    <div class="col-auto mt-2 mt-md-1 font-weight-bold" style="font-size: 19px">
+                                                        Vas Bunga Mini
+                                                    </div>
+                                                </div>
+                                                <div class="row d-flex justify-content-between align-items-center">
+                                                    <div class="col-auto">
+                                                        Size: S
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-4">
+                                                <div class="col-auto text-end font-weight-bold">
+                                                    Rp.40.000.00
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="row">
+                                            <div class="row my-1 d-flex justify-content-between align-items-center">
+                                                <div class="col-auto">
+                                                    Transaction Date
+                                                </div>
+                                                <div class="col-auto">
+                                                    Sunday, 19 May 2024
+                                                </div>
+                                            </div>
+                                            <div class="row my-1 d-flex justify-content-between align-items-center">
+                                                <div class="col-auto">
+                                                    Payment Method
+                                                </div>
+                                                <div class="col-auto">
+                                                    Bank Transfer
+                                                </div>
+                                            </div>
+                                            <div class="row my-1 d-flex justify-content-between align-items-center">
+                                                <div class="col-auto">
+                                                    Tax and Fees
+                                                </div>
+                                                <div class="col-auto">
+                                                    Rp.4000
+                                                </div>
+                                            </div>
+                                            <div class="row my-2 d-flex justify-content-between align-items-center" style="font-size: 19px">
+                                                <div class="col-auto font-weight-bold">
+                                                    Total
+                                                </div>
+                                                <div class="col-auto font-weight-bold" style="font-size: 19px">
+                                                    Rp.44.000.00
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
